fad2c23c29620554b5efa2bb521294888549eb8e
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-03-27 05:49:28 +0530

    Added : Ability to retrieve salary data for a particular employee as well as all employees who have joined in a particular data range from the payroll service database

--- src/EmployeeWage/connections/EmpWageJDBC.java
+++ src/EmployeeWage/connections/EmpWageJDBC.java
@@ -6,9 +6,9 @@ import java.sql.Connection;
 
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement;
 
 import EmployeeWage.controller.EmployeeWageMain;
 import EmployeeWage.model.EmployeeWagePojo;
@@ -26,19 +26,36 @@ public class EmpWageJDBC {
 		final String userName = "root";
 		final String password = "babayaga@12345";
 		Connection connection = DriverManager.getConnection(url,userName,password);
-		String query = "update employee_payroll set NetPay = ? where EmployeeName = ?;";
-
-		//Statement statement = connection.createStatement();
-		PreparedStatement statement = connection.prepareStatement(query);
-		statement.setInt(1, employeePojo.getNetPay());
-		statement.setString(2, employeePojo.getEmployeeName());
-
-
-		int res = statement.executeUpdate();
-		System.out.println(res+" :: records inserted");
-
+		
+		String query = "select * from employee_payroll where StartDate between cast('2015-01-01' as date) and date(now()); ";
+		
+		Statement statement = connection.createStatement();
+		
+		ResultSet resultSet = statement.executeQuery(query);
+		while(resultSet.next()) {
+			//int employeeID = resultSet.getInt("EmployeeID");
+			String employeeName = resultSet.getString("EmployeeName");
+			int netPay = resultSet.getInt("NetPay");
+			String startDate = resultSet.getString("StartDate");
+			String city = resultSet.getString("City");
+			String country = resultSet.getString("Country");
+			String address = resultSet.getString("Address");
+			String department = resultSet.getString("Department");
+			int basicPay = resultSet.getInt("BasicPay");
+			int deductions = resultSet.getInt("Deductions");
+			int taxAblePay = resultSet.getInt("TaxablePay");
+			int tax = resultSet.getInt("Tax");
+			
+			System.out.println("employeeID=" + ", employeeName=" + employeeName
+				+ ", netPay=" + netPay + ", startDate=" + startDate + ", city=" + city + ", country=" + country
+				+ ", address=" + address + ", department=" + department + ", basicPay="
+				+ basicPay + ", deductions=" + deductions + ", taxAblePay=" + taxAblePay + ", tax=" + tax);
+			
+		}
+		
 		statement.close();
 		connection.close();
+		
 
 	}
 }
